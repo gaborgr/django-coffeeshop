2940694d3fc63d5e3ebc8a94c7334371ee017fc1
gaborgr <gguerra.code@gmail.com>
2025-06-21 12:45:46 -0400

    Models Done

--- coffeeproject/products/models.py
+++ coffeeproject/products/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):  # Automated Slug
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
